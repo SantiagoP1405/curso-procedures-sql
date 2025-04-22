d85463c2294f971027c079718d1b463403a7ccff
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-04-21 20:14:17 -0600

    CURSOR CON LOOP
    - Modificación del cursor para trabajar con un ciclo while.

--- cursor.sql
+++ cursor.sql
@@ -5,17 +5,19 @@ DELIMITER $$
 USE `jugos_ventas`$$
 CREATE PROCEDURE `cursor_1` ()
 BEGIN
+DECLARE finc INT DEFAULT 0;
 DECLARE vnombre VARCHAR(50);
-DECLARE c CURSOR FOR SELECT NOMBRE FROM tabla_de_clientes LIMIT 4;
+DECLARE c CURSOR FOR SELECT NOMBRE FROM tabla_de_clientes;
+DECLARE CONTINUE HANDLER FOR NOT FOUND
+SET finc = 1;
 OPEN c;
+WHILE finc = 0
+DO
 FETCH c INTO vnombre; 
-SELECT vnombre;
-FETCH c INTO vnombre; 
-SELECT vnombre;
-FETCH c INTO vnombre; 
-SELECT vnombre;
-FETCH c INTO vnombre; 
-SELECT vnombre;
+IF finc = 0
+THEN SELECT vnombre;
+END IF;
+END WHILE;
 CLOSE c;
 END$$
 
